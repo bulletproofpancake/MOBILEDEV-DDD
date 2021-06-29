aedcab8794755f2062db66b2e1dde075df7e0edd
Xvyce <77040737+Xvyce@users.noreply.github.com>
2021-06-29 17:30:59 +0800

    removed some efx/ tested line renderer laser

---
+++ Assets/Scripts/Laser/Laser2.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser2 : MonoBehaviour
+{
+    public Camera mainCamera;
+    public LineRenderer _lineRenderer;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.mousePosition);
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Vector2 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            
+
+            _lineRenderer.SetPosition(1, transform.position);
+            _lineRenderer.SetPosition(0, mousePos);
+            
+
+            _lineRenderer.enabled = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+
+            _lineRenderer.enabled = false;
+        }
+
+    }
+}
