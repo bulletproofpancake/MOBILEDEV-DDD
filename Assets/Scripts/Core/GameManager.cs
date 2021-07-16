83fbd576156b0b75ec740a908256150a268b9bf6
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2021-07-16 16:47:18 +0800

    removed acceleration

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -42,8 +42,8 @@ namespace Core
             // returns when GameOver is called so that buildings stop moving
             if (isGameOver) return;
 
-            Time.timeScale += Time.deltaTime / bpm;
-            print(Time.timeScale);
+            // Time.timeScale += Time.deltaTime / bpm;
+            //print(Time.timeScale);
 
             // Time.deltaTime is divided by the weight so that it takes longer to reach the maximum rate
             // accelerationRate += Time.deltaTime / weight;
